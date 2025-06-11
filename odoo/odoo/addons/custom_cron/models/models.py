f60cbda1b754c7120d57e6b562058665f80e44b4
rohn21 <rohn2100@gmail.com>
2025-06-11 16:45:29 +0530

    added model

--- odoo/odoo/addons/custom_cron/models/models.py
+++ odoo/odoo/addons/custom_cron/models/models.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class custom_cron(models.Model):
     _name = 'custom.cron.model'
@@ -15,3 +18,23 @@ class custom_cron(models.Model):
             'name': 'Cron Executed',
             'last_run': fields.Datetime.now()
         })
+
+
+class SaleOrderCron(models.Model):
+    _inherit = 'sale.order'
+
+    @api.model
+    def auto_archive_old_sales(self):
+        days_threshold = 90
+        cutoff_date = fields.Date.to_string(datetime.today() - timedelta(days=days_threshold))
+
+        old_orders = self.search([
+            ('state', '=', 'sale'),
+            ('date_order', '<=', cutoff_date),
+        ])
+
+        if old_orders:
+            _logger.info(f"[CRON] Archiving {len(old_orders)} old confirmed sale orders (before {cutoff_date})")
+            old_orders.write({'name': lambda self: self.name + ' (Archived)'})
+        else:
+            _logger.info("[CRON] No old sale orders found to archive.")
