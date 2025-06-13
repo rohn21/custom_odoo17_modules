589c5eac0d50108bdee39b9fae717e6af14b9b59
rohn21 <rohn2100@gmail.com>
2025-06-13 18:47:19 +0530

    updated content

--- odoo/odoo/addons/qweb_tutorial/controllers/controllers.py
+++ odoo/odoo/addons/qweb_tutorial/controllers/controllers.py
@@ -3,11 +3,13 @@ from odoo import http
 from odoo.http import request
 import json
 
+
 class QwebTutorial(http.Controller):
     @http.route('/qweb-tutorial', auth='public', website=True)
     def index(self, **kw):
         return http.request.render('qweb_tutorial.qweb_html')
         # return "This is the tutorial page using controller"
+
 
 class TravelData(http.Controller):
     @http.route('/travel-data', auth='public', website=True)
@@ -16,7 +18,8 @@ class TravelData(http.Controller):
         output = {'data': retrieve_data}
         return http.request.render('qweb_tutorial.booking_details', output)
 
-# calendar
+
+# test-static-calendar
 class TravelCalendarController(http.Controller):
 
     @http.route('/travel/calendar', auth='public', website=True)
@@ -24,12 +27,15 @@ class TravelCalendarController(http.Controller):
         return request.render('qweb_tutorial.custom_calendar_page', {})
 
 
+# dynamic-events-calendar
 class TravelCalendarAPI(http.Controller):
 
-    @http.route('/travel/events', auth='public' , website=True)
+    @http.route('/calendar', auth='public', website=True)
     def travel_events(self, **kwargs):
-        events = request.env['travel.detail'].sudo().search([])
+        # events = request.env['travel.detail'].sudo().search([])
+        user_id = request.env.user.partner_id.id
+        appointments = request.env['custom.appointment'].sudo().search([('attendee_ids', 'in', [user_id])])
         # return request.redirect('/web#action=qweb_tutorial.action_travel_detail&view_type=calendar')
-        return request.render('qweb_tutorial.travel_event_calendar', {
-            'events': events,
+        return request.render('qweb_tutorial.event_calendar', {
+            'events': appointments,
         })
